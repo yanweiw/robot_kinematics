b0bdb91b8878910e43c8f8bde0f072c370ed4b18
nbfigueroa <nbfigueroa@gmail.com>
2020-02-15 23:08:23 -0500

    changes

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=[
-        "robot_kinematics_python",
-        "robot_kinematics_python.generic_kinematics_model",
-        "robot_kinematics_python.ur_kinematics_model",
+        "generic_kinematics_model",
     ],
     package_dir={'': 'robot_kinematics_python'},
 )
